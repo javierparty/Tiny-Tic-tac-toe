c0c6d07e2c267bcdda3b5255e6495528af7d9a4c
javierparty <javier.party@gmail.com>
2022-10-10 15:59:29 +0200

    Reduced modules

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
 import random as r
-import config as c
 import operations as op
 
 op.updateBoard('start')
-while c.possible_moves:
-    move = input("Your turn! ("+(', '.join(c.possible_moves))+"): ")
-    while move not in c.possible_moves:
+
+while op.possible_moves:
+    move = input("Your turn! ("+(', '.join(op.possible_moves))+"): ")
+    while move not in op.possible_moves:
         print('Invalid move!')
         op.updateBoard('start')
-        move = input("Your turn! ("+(', '.join(c.possible_moves))+"): ")
+        move = input("Your turn! ("+(', '.join(op.possible_moves))+"): ")
     else:
         op.updateBoard(move)
-    if len(c.player_moves) > 2:
-        if op.checkIfPlayerWon(c.player_moves):
+    if len(op.player_moves) > 2:
+        if op.checkIfPlayerWon(op.player_moves):
             print('You won! (Lucky human)')
             exit()
-        elif bot_move := op.botWinsOrBlocks(c.bot_moves):
+        elif bot_move := op.botWinsOrBlocks(op.bot_moves):
             op.updateBoard(bot_move)
             print('I won!')
             exit()
-    if len(c.player_moves) > 1:
-        if block := op.botWinsOrBlocks(c.player_moves):
+    if len(op.player_moves) > 1:
+        if block := op.botWinsOrBlocks(op.player_moves):
             bot_move = block
         else:
-            bot_move = op.goodMove(c.bot_moves)
+            bot_move = op.goodMove(op.bot_moves)
         op.updateBoard(bot_move)
     else:
         if move == 'b2':
-            bot_move = r.choice(list(c.corners))
+            bot_move = r.choice(list(op.corners))
         else:
             bot_move = 'b2'
         op.updateBoard(bot_move)
-    if len(c.player_moves) == 4:
+    if len(op.player_moves) == 4:
         print('You cannot win, human!')
         exit()
--- operations.py
+++ operations.py
@@ -1,5 +1,10 @@
-import config as c
 import random as r
+
+possible_moves = {'a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3'}
+e = {m: ' ' for m in possible_moves}
+corners = {'a1', 'a3', 'c1', 'c3'}
+player_moves = set()
+bot_moves = set()
 
 
 def checkIfPlayerWon(moves):
@@ -26,22 +31,22 @@ def botWinsOrBlocks(moves):
                     nm_col = '123'.replace(m_1[1:2], '')
                     nm_col = nm_col.replace(m_2[1:2], '')
                     next_move = m_1[0:1] + nm_col
-                    if next_move in c.possible_moves:
+                    if next_move in possible_moves:
                         return next_move
                 elif m_1[1:2] == m_2[1:2]:
                     nm_line = 'abc'.replace(m_1[0:1], '')
                     nm_line = nm_line.replace(m_2[0:1], '')
                     next_move = nm_line + m_1[1:2]
-                    if next_move in c.possible_moves:
+                    if next_move in possible_moves:
                         return next_move
-                elif ((m_1 == 'b2' and m_2 in c.corners) or
+                elif ((m_1 == 'b2' and m_2 in corners) or
                         ''.join(sorted(m_1+m_2)) == '13ac'):
                     nm_col = '123'.replace(m_1[1:2], '')
                     nm_col = nm_col.replace(m_2[1:2], '')
                     nm_line = 'abc'.replace(m_1[0:1], '')
                     nm_line = nm_line.replace(m_2[0:1], '')
                     next_move = nm_line + nm_col
-                    if next_move in c.possible_moves:
+                    if next_move in possible_moves:
                         return next_move
     return False
 
@@ -49,42 +54,42 @@ def botWinsOrBlocks(moves):
 def goodMove(moves):
     good_moves = set()
     for m in moves:
-        if len(c.player_moves) == 2 and 'b2' in c.player_moves:
-            player_move_2 = ''.join(c.player_moves).replace('b2', '')
-            if player_move_2 in c.corners and m in c.corners:
-                for next_move in c.corners:
-                    if next_move in c.possible_moves:
+        if len(player_moves) == 2 and 'b2' in player_moves:
+            player_move_2 = ''.join(player_moves).replace('b2', '')
+            if player_move_2 in corners and m in corners:
+                for next_move in corners:
+                    if next_move in possible_moves:
                         return next_move
         elif m == 'b2':
-            if {'a1', 'c3'}.issubset(c.possible_moves):
+            if {'a1', 'c3'}.issubset(possible_moves):
                 return r.choice(['a1', 'c3'])
-            elif {'a3', 'c1'}.issubset(c.possible_moves):
+            elif {'a3', 'c1'}.issubset(possible_moves):
                 return r.choice(['a3', 'c1'])
-        if m in c.corners:
+        if m in corners:
             good_moves = {'b2', 'ac13'.replace(m[0:1], '').replace(m[1:2], '')}
-            if good_moves.issubset(c.possible_moves):
+            if good_moves.issubset(possible_moves):
                 return r.choice(list(good_moves))
         gm_cols = '123'.replace(m[1:2], '')
         good_moves = {m[0:1] + gm_cols[0:1], m[0:1] + gm_cols[1:2]}
-        if good_moves.issubset(c.possible_moves):
+        if good_moves.issubset(possible_moves):
             return r.choice(list(good_moves))
         gm_lines = 'abc'.replace(m[0:1], '')
         good_moves = {gm_lines[0:1] + m[1:2], gm_lines[1:2] + m[1:2]}
-        if good_moves.issubset(c.possible_moves):
+        if good_moves.issubset(possible_moves):
             return r.choice(list(good_moves))
-    return r.choice(list(c.possible_moves))
+    return r.choice(list(possible_moves))
 
 
 def updateBoard(last_move):
     if last_move != 'start':
-        if len(c.possible_moves) % 2 == 0:
-            c.e[last_move] = 'X'
-            c.bot_moves.add(last_move)
+        if len(possible_moves) % 2 == 0:
+            e[last_move] = 'X'
+            bot_moves.add(last_move)
         else:
-            c.e[last_move] = 'O'
-            c.player_moves.add(last_move)
-        c.possible_moves.remove(last_move)
+            e[last_move] = 'O'
+            player_moves.add(last_move)
+        possible_moves.remove(last_move)
     print("\n   1   2   3\n" +
-          "a _" + c.e['a1'] + "_|_" + c.e['a2'] + "_|_" + c.e['a3'] + "_\n" +
-          "b _" + c.e['b1'] + "_|_" + c.e['b2'] + "_|_" + c.e['b3'] + "_\n" +
-          "c  " + c.e['c1'] + " | " + c.e['c2'] + " | " + c.e['c3'] + " \n")
+          "a _" + e['a1'] + "_|_" + e['a2'] + "_|_" + e['a3'] + "_\n" +
+          "b _" + e['b1'] + "_|_" + e['b2'] + "_|_" + e['b3'] + "_\n" +
+          "c  " + e['c1'] + " | " + e['c2'] + " | " + e['c3'] + " \n")
